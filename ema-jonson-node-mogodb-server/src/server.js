b15f28c2317e6da94265094e69cc51f1e6069662
anamerasul <anamerasul@gmail.com>
2022-04-25 02:02:36 +0600

    HTTP Method, Get VS POST, connect mongodb

--- ema-jonson-node-mogodb-server/src/server.js
+++ ema-jonson-node-mogodb-server/src/server.js
@@ -25,122 +25,34 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lvjq0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    // const collection = client.db("test").collection("devices");
-    const UsersCollection = client.db("emajonsonNodeMongo").collection("users");
-    console.log("db connected")
-    // perform actions on the collection object
-    // client.close();
-});
+// client.connect(err => {
+//     // const collection = client.db("test").collection("devices");
+//     const productsCollection = client.db("emajonsonNodeMongo").collection("products");
+//     console.log("db connected")
+//     // perform actions on the collection object
+//     // client.close();
+// });
 
 
-// const run = async () => {
-//     try {
-//         await client.connect();
-//         console.log("db connected1");
-
-//         const notesTakerNodeCollection = client.db("notestakernode").collection("notes");
-
-
-
-//         // get api to read all users
-
-//         app.get('/notes', async (req, res) => {
-//             // console.log(req);
-
-//             const query = req.query
-//             console.log(query);
-
-//             const cursor = notesTakerNodeCollection.find(query)
-
-//             const results = await cursor.toArray()
-
-//             res.send(results)
-
-//         })
-
-//         //create notesTaker
-//         app.post('/notes', async (req, res) => {
-//             const data = req.body
-//             console.log(data);
-
-//             const result = await notesTakerNodeCollection.insertOne(data)
-
-//             res.send(result)
-//         })
-
-
-//         // update notes
-
-//         app.put('/notes/:id', async (req, res) => {
-
-//             const id = req.params.id
-
-
-//             // const query = { _id: ObjectId(id) };
-
-//             const filter = { _id: ObjectId(id) }
-//             const data = req.body
-
-//             console.log("from update", data)
-
-//             const options = { upsert: true };
-
-//             const updateNotes = {
-//                 $set: {
-//                     // plot: `A harvest of random numbers, such as: ${Math.random()}`
-//                     userName: data.userName,
-//                     testData: data.testData,
-//                 },
-//             };
-
-//             console.log("from put", id)
-
-//             const result = await notesTakerNodeCollection.updateOne(filter, updateNotes, options);
-
-//             res.send(result)
-
-//         })
-
-
-//         // delete notes
-
-//         app.delete('/notes/:id', async (req, res) => {
-
-//             const id = req.params.id
-
-//             const data = req.body
-//             const query = { _id: ObjectId(id) };
+const run = async () => {
+    try {
+        await client.connect();
+        const productsCollection = client.db("emajonsonNodeMongo").collection("products");
+        console.log("db connected to mongo")
 
 
 
-//             console.log("from delete", data)
-
-//             // const options = { upsert: true };
-
-//             const result = await notesTakerNodeCollection.deleteOne(query);
-
-//             if (result.deletedCount === 1) {
-//                 console.log("Successfully deleted one document.");
-//                 res.send(result)
-//             } else {
-//                 console.log("No documents matched the query. Deleted 0 documents.");
-//                 res.send(result)
-//             }
-
-//         })
 
 
+    }
 
-//     }
 
+    finally {
+        // await client.close();
+    }
 
-//     finally {
-//         // await client.close();
-//     }
-
-// }
-// run().catch(console.dir);
+}
+run().catch(console.dir);
 
 
 
